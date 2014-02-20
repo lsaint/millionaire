2a3c8f4c7aa964eb77cdab20ab429c73f6a94781
lSaint <ls4int@gmail.com>
2014-02-20 17:14:28 +0800

    state

--- script/room.py
+++ script/room.py
@@ -2,6 +2,7 @@
 
 from timer import Timer
 from sender import Sender
+from state import *
 from player import Player
 from award import AwardChecker
 from match import g_match_mgr
@@ -119,6 +120,10 @@ class Room(Sender):
         return user.uid == self.presenter.uid
 
 
+    def GetCurRightAnswer(self):
+        return self.qpackage.GetRightAnswer(self.cur_qid)
+
+
     def GetSurvivorNum(self):
         num = 0
         for uid, player in self.uid2player.iteritems():
@@ -133,169 +138,5 @@ class Room(Sender):
 
     def PrizeGiving(self):
         return self.achecker.PrizeGiving(self.cur_qid)
-
-
-#
-class IdleState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pass
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.ready_state)
-
-
-#
-class ReadyState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pass
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.timing_state)
-
-
-#
-class TimingState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pb = L2CNotifyTimingStatus()
-        pb.question = self.room.GenNextQuestion()
-        pb.start_time = self.room.cur_q_start_time
-        self.room.Randomcast(pb)
-        self.stati = StatiMgr(self.room.qpackage.GetRightAnswer(self.cur_qid))
-
-
-    def OnAnswerQuestion(self, ins):
-        if ins.answer.id != self.room.cur_qid:
-            return
-        player = self.GetPlayer(ins.answer.user.uid)
-        if not player:
-            return
-        if not player.DoAnswer(ins.answer.id, ins.answer.answer):
-            return
-        self.stati.OnAnswer(player, ins.answer.answer, int(time.time()) - self.cur_q_start_time)
-
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.timeup_state)
-
-
-#
-class TimeupState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pb = L2CNotifyTimeupStatus()
-        self.room.Randomcast(pb)
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.statistics_state)
-
-
-#
-class StatisticsState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pb = L2CNotifyStatisticsStatus()
-        pb.stati.extend(self.room.stati.GetDistribution())
-        pb.sections = self.room.CheckCurRaceAward()
-        self.room.Randomcast(pb)
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.answer_state)
-
-
-#
-class AnswerState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pb = L2CNotifyAnswerStatus()
-        pb.right_answer = self.room.qpackage.GetRightAnswer(self.cur_qid)
-        self.room.Randomcast(pb)
-
-
-    def OnNextStep(self, ins):
-        _, exist = self.room.CheckCurRaceAward()
-        if exist:
-            self.room.SetState(self.room.award_state)
-        else:
-            self.room.SetState(self.room.timing_state)
-
-
-
-
-#
-class AwardState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        self.room.PrizeGiving()
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.ending_state)
-
-
-
-#
-class AnnounceState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pass
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.award_state)
-
-
-#
-class EndingState(object):
-
-    def __init__(self, room):
-        self.room = room
-
-
-    def OnEnterState(self):
-        pass
-
-
-    def OnNextStep(self, ins):
-        self.room.SetState(self.room.idle_state)
 
 
---
+++ script/state.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+
+
+class IdleState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pass
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.ready_state)
+
+
+#
+class ReadyState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pass
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.timing_state)
+
+
+#
+class TimingState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pb = L2CNotifyTimingStatus()
+        pb.question = self.room.GenNextQuestion()
+        pb.start_time = self.room.cur_q_start_time
+        self.room.Randomcast(pb)
+        self.stati = StatiMgr(self.room.GetCurRightAnswer())
+
+
+    def OnAnswerQuestion(self, ins):
+        if ins.answer.id != self.room.cur_qid:
+            return
+        player = self.GetPlayer(ins.answer.user.uid)
+        if not player:
+            return
+        if not player.DoAnswer(ins.answer.id, ins.answer.answer):
+            return
+        self.stati.OnAnswer(player, ins.answer.answer, int(time.time()) - self.cur_q_start_time)
+
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.timeup_state)
+
+
+#
+class TimeupState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pb = L2CNotifyTimeupStatus()
+        self.room.Randomcast(pb)
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.statistics_state)
+
+
+#
+class StatisticsState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pb = L2CNotifyStatisticsStatus()
+        pb.stati.extend(self.room.stati.GetDistribution())
+        pb.sections = self.room.CheckCurRaceAward()
+        self.room.Randomcast(pb)
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.answer_state)
+
+
+#
+class AnswerState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pb = L2CNotifyAnswerStatus()
+        pb.right_answer = self.room.GetRightAnswer()
+        self.room.Randomcast(pb)
+
+
+    def OnNextStep(self, ins):
+        _, exist = self.room.CheckCurRaceAward()
+        if exist:
+            self.room.SetState(self.room.award_state)
+        else:
+            self.room.SetState(self.room.timing_state)
+
+
+
+
+#
+class AwardState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        self.room.PrizeGiving()
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.ending_state)
+
+
+
+#
+class AnnounceState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pass
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.award_state)
+
+
+#
+class EndingState(object):
+
+    def __init__(self, room):
+        self.room = room
+
+
+    def OnEnterState(self):
+        pass
+
+
+    def OnNextStep(self, ins):
+        self.room.SetState(self.room.idle_state)
+
+

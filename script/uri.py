8d87a096c7460ca1bb5433b8b8f9c81d667891a5
lSaint <ls4int@gmail.com>
2014-02-22 18:07:23 +0800

    uri

--- script/uri.py
+++ script/uri.py
@@ -6,7 +6,33 @@ from server_pb2 import *
 
 
 URI2CLASS = {
-        1   :   FrontendPack,
+        100     :   C2LMatchInfo,
+        101     :   L2CMatchInfoRep,
+        102     :   C2LStartMatch,
+        103     :   L2CNotifyMatchInfo,
+        104     :   C2LLogin,
+        105     :   L2CLoginRep,
+        106     :   C2LTimeSync,
+        107     :   L2CTimeSyncRep,
+        108     :   C2LPing,
+        109     :   L2CPingRep,
+        110     :   F2LNotifyMic1,
+        111     :   C2LNextStep,
+        112     :   L2CNextStepRep,
+        113     :   L2CNotifyPresenterChange,
+        114     :   L2CNotifyReadyStatus,
+        115     :   L2CNotifyTimingStatus,
+        116     :   L2CNotifyTimeupStatus,
+        117     :   C2LAnswerQuesion,
+        118     :   L2CNotifyStatisticsStatus,
+        119     :   L2CNotifyAnswerStatus,
+        120     :   L2CNotifyAnnounceStatus,
+        121     :   L2CNotifyAwardStatus,
+        122     :   L2CNotifyEndingStatus,
+        123     :   C2FRevive,
+        124     :   F2CReviveRep,
+        125     :   F2LNotifyRevive,
+        126     :   L2FNotifyRevieRep,
 }
 
 CLASS2URI = {}

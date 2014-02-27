0ba408665bbd44bcf0f5b4af00f698512d657a88
lSaint <ls4int@gmail.com>
2014-02-27 16:34:41 +0800

    log send proto name

--- script/sender.py
+++ script/sender.py
@@ -23,17 +23,17 @@ class Sender(object):
     def Broadcast(self, ins):
         uri, bin = self.parse(ins)
         go.SendMsg(self.tsid, self.ssid, uri, bin, server_pb2.Broadcast, 0)
-        logging.debug("<--Broadcast: %s" % ins)
+        logging.debug("<--Broadcast %s: %s" % (ins.DESCRIPTOR.name, ins))
 
 
     def Randomcast(self, ins):
         uri, bin = self.parse(ins)
         go.SendMsg(self.tsid, self.ssid, uri, bin, server_pb2.Randomcast, 0)
-        logging.debug("<--Randomcast: %s" % ins)
+        logging.debug("<--Randomcast %s: %s" % (ins.DESCRIPTOR.name, ins))
 
 
     def SpecifySend(self, ins, uid):
         uri, bin = self.parse(ins)
         go.SendMsg(self.tsid, self.ssid, uri, bin, server_pb2.Specify, uid)
-        logging.debug("<--SpecifySend %d: %s" % (uid, ins))
+        logging.debug("<--SpecifySend %d %s: %s" % (uid, ins.DESCRIPTOR.name, ins))
 

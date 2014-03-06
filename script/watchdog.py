adef5ed99be342a01d4169ea4784d7a4fa50c1c3
lSaint <ls4int@gmail.com>
2014-03-06 14:26:47 +0800

    igonre

--- script/watchdog.py
+++ script/watchdog.py
@@ -32,8 +32,7 @@ class WatchDog(object):
             return
         room = self.gainRoom(tsid, ssid)
         method_name= "%s%s" % ("On", ins.DESCRIPTOR.name[3:])
-        logging.debug("dispatch %s:" % method_name)
-        logging.debug(str(ins))
+        logging.debug("dispatch %s: %s" % (method_name, str(ins)))
         try:
             method = getattr(room, method_name)
         except:

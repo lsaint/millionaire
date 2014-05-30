371b8d49cca38dfdca4355e802bab8bbfb063482
lSaint <ls4int@gmail.com>
2014-05-30 11:37:03 +0800

    change timeup desc

--- script/flag.py
+++ script/flag.py
@@ -253,9 +253,10 @@ class FlagMgr(Sender):
         self.changeDoneAction(Defended)
         s = ""
         if self.owner.uid != 0:
-            s = u"恭喜你最终成功守护战旗，你将获得7天的战旗拥有权，以及71频道的独家冠名权。"
+            format = u"恭喜%s最终成功守护战旗，%s将获得7天的战旗拥有权，以及71频道的独家冠名权。"
+            s = format % (u"你", u"你")
             self.notifyFlagMessage(Popup, s, None, self.owner.uid)
-            s = u"恭喜%s在战旗攻防战中一夫当关，坚持到最后，大家祝贺TA！" % self.owner.name
+            s = format % (self.owner.name, u"并")
             self.notifyFlagMessage(PopupUid, s, self.owner.uid)
         else:
             s = u"夺旗结束，各路大侠摩拳擦掌准备着下一轮的战斗，好戏即将上演！"
